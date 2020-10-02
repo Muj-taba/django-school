d1392de3a0352f304f90ff21e6b57562d6f85c6b
Mujtaba Altayib <e.1p@yahoo.com>
2020-10-02 13:00:30 +0400

    add project

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+from blog.views import all_posts , single_post , new_post , PostList
+#have to import functions to declare 
+
+app_name='blog'
+
+urlpatterns = [
+    path('' , all_posts , name='blog_list'),
+    path('<int:id>',single_post, name='blog_detail'),
+    path('new' , new_post , name='new_post'), #new is a name of html page
+
+
+
+    #CBV
+    path('cbv',PostList.as_view(), name='all_post_cbv'),
+]
---
+++ blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect,reverse
+from .models import Post
+from .forms import PostForm
+from django.views.generic import ListView , DetailView
+# Create your views here.
+
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog_list.html'
+
+
+
+#show all posts
+def all_posts(request):
+    all_posts =Post.objects.all()
+   
+    return render(request , 'post/all_posts.html' ,{'posts':all_posts} )
+
+#show single post
+def single_post(request,id):
+    single_post = Post.objects.get(id=id)
+    return render(request , 'post/single_post.html' ,{'post':single_post} )
+
+#add post
+def new_post(request):
+  #  print('nnn') # to test data
+    if request.method=='POST':
+        form = PostForm(request.POST , request.FILES) #Request.files to recieve data (audio, imgs, videos)
+        if form.is_valid():
+           form.save()
+           return redirect(reverse('blog:blog_list'))
+    else:
+        form = PostForm()
+    return render(request, 'post/new.html' , {'form':form})
